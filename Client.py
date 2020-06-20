1e7ff78fe699c1087a6603e02714787d3c4e1bb6
Mellumynx <20rkoda@shcp.edu>
2020-06-19 17:55:58 -0700

    Clean up server.py; Fixed hardcode token and local_dir

--- Client.py
+++ Client.py
@@ -1,59 +1,133 @@
 #!/usr/bin/python
 import time
-
+import os
 import requests
-
+import shutil
+import tempfile
+import difflib
+import argparse
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--token', required=True)
+    parser.add_argument('--local_dir', required=True)
+    args = parser.parse_args()
+    print(args.token)
+    print(args.local_dir)
     while(True):
-        time.sleep(5)
-        sync()
+        time.sleep(2)
+        sync(args.token, args.local_dir)
 
-def sync():
-    URL = "http://127.0.0.1:5000/getAllFiles"
-    headers = {'Authorization' : 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRva2VuIiwidGltZXN0YW1wIjoiMTItSnVuLTIwMjAgKDE3OjMwOjI2Ljc5MjE2MikifQ.zablyO9VkZg0fFRNImGsgH1sJiccn4DKlNsFIh9DXAk'}
+def run_cmp(filename1, filename2):
+    cmd = 'cmp --verbose %s %s'%(filename1, filename2)
+    stream = os.popen(cmd)
+    output = stream.read()
+    return len(output)
+
+
+def sync(token, dir):
+    # remote, 
+    URL = "http://127.0.0.1:5000/getAllFiles" 
+    headers = {'Authorization' : 'Bearer ' + token}
+    # use url, token to get request.
     r = requests.get(url = URL, headers=headers)
-    # class time: 1. comparison (if statement) 
-    # how to get current modifed time by file 
-
     data = r.json()
-    print(data)
 
+    # if there is no result -> "no result found", else store results in file_List_Remote
+    if "result" not in data: 
+        return "No result found"
+    file_List_Remote = data["result"]
+
+    # local
+    local_Folder = dir
+    file_List_Local = []
+    for filename in os.listdir(local_Folder):
+        # ignore all files start with '.'
+        if filename[0] == '.':
+            continue
+        # get modified time for each file
+        mtime = os.path.getmtime(os.path.join(local_Folder,filename))
+        # add to file_List_Local
+        file_List_Local.append({"filename": filename, "mtime": mtime})
+
+    # compare
+    intersectFiles = []
+    # nested for loop, select files in both file_List_Remote and file_List_Local
+    for localfile in file_List_Local: 
+        for remotefile in file_List_Remote: 
+            if localfile["filename"] == remotefile["filename"]:
+                intersectFiles.append(localfile["filename"])
     
+    # upload/download
+    filesToUpload = []
+    filesToDownload = []
+    for localfile in file_List_Local: 
+        if localfile["filename"] not in intersectFiles:
+            filesToUpload.append(localfile["filename"])
+
+    for remotefile in file_List_Remote:
+        if remotefile["filename"] not in intersectFiles:
+            filesToDownload.append(remotefile["filename"])
+    
+    # for every file, get modified time for both local and remote 
+    for f in intersectFiles:
+        local_time = 0
+        remote_time = 0
+        for localFileObject in file_List_Local: 
+            if localFileObject["filename"] == f: 
+                local_time = localFileObject["mtime"]
+                break
+
+        for remoteFileObject in file_List_Remote: 
+            if remoteFileObject["filename"] == f: 
+                remote_time = remoteFileObject["mtime"]
+                break
         
-    # use token - > get all files
-    # compare with local files
-    # if only one file exist, use file
-    # if not (time same) = no change
-    #        (time different) = timestamp (new) 
+        # make a comparison whether to upload/download
+        if local_time > remote_time:
+            filesToUpload.append(f)
 
-    """
-    1. first use : cloud no file / local has file = current api (check)
-    2. second + use : update existing file = upload every 5 mins (no) ??
-    3. new local client : cloud has file / new local no file (no)
-    4. different client communication (no)
+        elif local_time < remote_time:
+            filesToDownload.append(f) 
 
-    Timestamp: upload, edited ??
-    """
+   
+    #print("Files to upload: " + ','.join(filesToUpload))
+    #print("Files to download: " + ','.join(filesToDownload))
 
-        # timestamp (pacific)
-        # support multiple token = username + ip
 
-    # note to self: (api action time limit: 5 min when active) = token list
-        # check user (activation)  ~ different ip / remove existing
-        # get full list from server
-        # compare list (to local)
-            # if equal
-                # (check)
-            # elif (no such document) docuemnt id (new variable)
-                # upload/download accorindly
-            # else 
-                # check "modified time" (new variable) = upload/download accordingly 
-                # if local modified time < server modified time: download & replace new version
-                # if server modifed < local modifed time: upload 
+    upload = "http://127.0.0.1:5000/upload"
+    for actionFile in filesToUpload: 
+        # open (file)
+        with open(os.path.join(local_Folder, actionFile), 'rb') as f:
+            u = requests.post(upload, headers = headers, files = {"file": f})
+            print("Upload " + actionFile + ':' + str(u.content))
+    
+    download = "http://127.0.0.1:5000/download"
+    for actionFile in filesToDownload: 
+        d = requests.get(download, headers = headers, params = {"filename": actionFile})
+        content = d.content
+
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            #print('created temporary directory', tmpdirname)
         
+            with open(os.path.join(tmpdirname, actionFile), 'wb') as location:
+                location.write(content)
+                #print("Downloaded " + filename)
+
+            # print("diffbyte:" + str(run_cmp(os.path.join(local_Folder, actionFile), os.path.join(tmpdirname, actionFile))))
+
+            if not os.path.isfile(os.path.join(local_Folder, actionFile)) or run_cmp(os.path.join(local_Folder, actionFile), os.path.join(tmpdirname, actionFile)) != 0: 
+                shutil.copyfile(os.path.join(tmpdirname, actionFile), os.path.join(local_Folder, actionFile))
+                print("localfile updated: " + actionFile)
+            
 
 
+            # if run_cmp (local file not exist) = save
+            # local file exist (diff) = save
+            # local file exist (same) = dont save
+
+            #design document 大体描述 环境 1.5k
+            
 
 
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -12,38 +12,45 @@ from werkzeug.utils import secure_filename
 
 import socket
 
+# key for token
 key = 'secret'
 
-
+# ?
 app = Flask(__name__)
 
 
-
+# route '/' 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
-@app.route('/register', methods=['GET', 'POST'])
+# route '/register' methods include POST'
+@app.route('/register', methods=['POST'])
 def register():
+    # initialize variables
     result = ""
     status = "ERROR"
-    if request.method == "GET":
-        return jsonify({
-            "User": "username",
-            "Pwd": "password"
-        })
-    elif request.method == "POST":
+
+    if request.method == "POST":
+        # acquire initial user inputs 
         requestJson = request.json
         username = requestJson['Username']
         password = requestJson['Password']
         confirmation = requestJson['Confirm']
 
+        # connect to database "drive"
         cnx = mysql.connector.connect(user="frover", password="frover", host="34.67.158.25", database="drive")
         cursor = cnx.cursor()
+        # sql statement, select all users 
         query = "SELECT * FROM users;"
         cursor.execute(query)
         
         for (_, u, p, _,_,_, _) in cursor: 
+            """
+            run the for loop to check whether the new registered username has been in the database or not: 
+                if username input corresponds with correct password -> account exist 
+                if username input doesn't correspond with correct password -> username has been taken
+            """
             if u == username:
                 if p == password: 
                     result = "Account already exist."
@@ -57,118 +64,112 @@ def register():
                         result = result,
                         status = status
                     )
-
+        # if username is new, check whether the password string matches the confirmation string.
         if password == confirmation: 
+            # if it does, sql statement (add new user)
             add_Data = "INSERT INTO users (username, password) VALUES ('" + username + "', '" + password + "');"
             cursor.execute(add_Data)
             result = "Succesfully registered"
             status = "OK"
             
-        else: 
+        else:
+            # if not, return a statement 
             result = "Password does not match"
-            
+
+        # close connection with database    
         cnx.commit()    
         cursor.close()
         cnx.close()
 
+    # return final statement
     return jsonify(
         result = result,
         status = status
     )
 
-@app.route('/signin', methods=['GET', 'POST'])
+# route '/signin' methods include POST
+@app.route('/signin', methods=['POST'])
 def signin():
+    # initialize variables
     result = "Fail"
+    token = ""
+    tokenList = []
+
+    # acquire user signin inputs 
     requestJson = request.json
     username = requestJson['Username']
     password = requestJson['Password']
-    token = ""
-    tokenList = []
-    
 
     if request.method == "POST":
+        # connect to database "drive"
         cnx = mysql.connector.connect(user="frover", password="frover", host="34.67.158.25", database="drive")
         cursor = cnx.cursor() 
+        # sql statement select specific user in database
         query = "SELECT * FROM users WHERE username = '" + username + "' ;"
         cursor.execute(query)
         
+        # string a current datetime object
         now = datetime.now().strftime("%d-%b-%Y (%H:%M:%S.%f)")
-        for (_, u, p, t,_, _, tl) in cursor: 
-            print(u)
-            print(p)
-            if p == password: 
-                """
-                #ip
-                hostname = socket.gethostname()
-                ip_address = socket.gethostbyname(hostname)
-                print(f"Hostname: {hostname}")
-                print(f"IP Address: {ip_address}")
-                """
-                
 
-                #sql, append token = token list
+        # for loop, check every user whether input password ?
+        for (_, u, p, t,_, _, tl) in cursor: 
+            if p == password: 
+                #sql, append token to tokenlist everytime assigned
                 encoded = jwt.encode({"username": username, "timestamp": now}, key, algorithm='HS256')
                 
+                # set empty token list
                 tlt = tl
                 if not tlt: 
                     tlt = ""
+
+                # sql statement, add new token to tokenlist
                 setToken = "UPDATE users SET tokenList = '" + tlt + ";" + encoded + "' WHERE username = '" + u + "';"
-                
-                # setDate = "UPDATE users SET modified_time = CURRENT_TIMESTAMP WHERE username = '" + u + "';"
-                # setIp = "UPDATE users SET ip = '" + ip "' WHERE username = '" + u + "';"
-                # print(setIp)
                 cursor.execute(setToken) 
-                # cursor.execute(setDate)
-                #cursor.execute(setIp)
+
+                # commit sql statement
                 cnx.commit()
                 result = "Success"
-                token = encoded
                 break
 
+        # close connection with database   
         cursor.close()
         cnx.close()
 
+    # return final statement
     return jsonify(
         token = token,
         status = result
     )
 
-
-
-# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-# UPLOAD = '/Users/apple/Desktop/'
+# set remote folder
 UPLOAD_FOLDER = '/Users/apple/Desktop/drive_root_dir/'
+# allowed file types
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
-"""
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-"""
-
+# route '/upload' methods include POST
 @app.route('/upload', methods=['GET', 'POST'])
 def upload(): 
-    # Authorization (token)
+    # initialize variables
     status = "ERROR"
+
+    # Authorization (token) ?
     if "Authorization" not in request.headers:
         return jsonify(
             status = status
         )
     token = request.headers["Authorization"][7:]
-    print(token)
 
-    # Search user
+    # connect to database "drive"
     cnx = mysql.connector.connect(user="frover", password="frover", host="34.67.158.25", database="drive")
     cursor = cnx.cursor() 
-    # query = "SELECT username FROM users WHERE token = '" + token + "';"
+    # use token for every functions
     query = "SELECT username FROM users WHERE INSTR(tokenList, '" + token + "');"
     cursor.execute(query)
     
+    # check if there is only one user 
     count = 0 
     username = ""
     for u in cursor: 
-        print(u)
         count = count + 1
         username = u[0]
     if count < 1:
@@ -182,26 +183,31 @@ def upload():
             status = status
         )
 
-    # Post selected file
     if request.method == "POST":
+        # if file does not exist -> "no files present"
         if 'file' not in request.files:
             return jsonify(
                 result = "No files present",
                 status = status
             )
-
+        
+        # if empty filename, -> "empty filename"
         file = request.files['file']
         if file.filename == '':
             return jsonify(
                 result = "Empty filename",
                 status = status
             )
-        if file: # each person needs each own directory 
+        
+        # if file exist
+        if file: 
+            # each person needs each own directory 
             usernamedir = os.path.join(UPLOAD_FOLDER, username)
-            print(usernamedir)
+            # if file directory does not exist, create such directory using username
             if not os.path.isdir(usernamedir):
                 os.makedirs(usernamedir)
             
+            # upload file
             filename = secure_filename(file.filename)
             file_path = os.path.join(usernamedir, filename)
             file.save(file_path)
@@ -216,24 +222,28 @@ def upload():
         status = status
     )
 
-
-@app.route('/getAllFiles', methods=['GET', 'POST'])
+# route '/getAllFiles' methods include GET
+@app.route('/getAllFiles', methods=['GET'])
 def getAllFiles():
+    # initialize variables
     status = "ERROR"
+    file_list = []
+
      # Authorization (token)
     if "Authorization" not in request.headers:
         return jsonify(
             status = status
         )
     token = request.headers["Authorization"][7:]
-    print(token)
 
-    # get username
+    # connect to database "drive"
     cnx = mysql.connector.connect(user="frover", password="frover", host="34.67.158.25", database="drive")
     cursor = cnx.cursor() 
+    # use token for every functions
     query = "SELECT username FROM users WHERE INSTR(tokenList, '" + token + "');"
     cursor.execute(query)
 
+    # check if there is only one user 
     count = 0 
     username = ""
     for u in cursor: 
@@ -252,13 +262,14 @@ def getAllFiles():
         )
 
     # Return all files from given directory
-    file_list = []
     if request.method == "GET":
-        #json list files
+        # select directory
         usernamedir = os.path.join(UPLOAD_FOLDER, username)
+        # for loop to get all files, and if file name starts with '.' ignore it
         for filename in os.listdir(usernamedir):
+            if filename[0] == '.':
+                continue
             mtime = os.path.getmtime(os.path.join(usernamedir,filename))
-            print(mtime)
             file_list.append({"filename": filename, "mtime": mtime})
     
     status = "OK"
@@ -267,22 +278,27 @@ def getAllFiles():
         status = status
     )
 
+# route '/download' methods include GET
 @app.route('/download', methods=['GET', 'POST'])
 def download(): 
-      # Authorization (token)
+    # initialize variables 
     status = "ERROR"
+
+    # Authorization (token)
     if "Authorization" not in request.headers:
         return jsonify(
             status = status
         )
     token = request.headers["Authorization"][7:]
 
-    # get username
+    # connect to database "drive"
     cnx = mysql.connector.connect(user="frover", password="frover", host="34.67.158.25", database="drive")
     cursor = cnx.cursor() 
+    # use token for every functions
     query = "SELECT username FROM users WHERE INSTR(tokenList, '" + token + "');"
     cursor.execute(query)
 
+    # check if there is only one user 
     count = 0 
     username = ""
     for u in cursor: 
@@ -299,22 +315,23 @@ def download():
             status = status
         )
 
-    requestForm = request.form
-    if "filename" not in requestForm:
+    # connect to client, request filename
+    filename = request.args.get("filename")
+    if not filename or len(filename) == 0: 
         return jsonify(
-            result = "Filename not found.",
+            result = "Filename is not specified.",
             status = status
         )
-    filename = requestForm['filename']
 
+    # Use directory to download
     if request.method == "GET":
         path = os.path.join(UPLOAD_FOLDER, username)
         return send_from_directory(path, filename, as_attachment=True)
 
     
     return jsonify(
-            status = status
-        )
+        status = status
+    )
 
 
 
